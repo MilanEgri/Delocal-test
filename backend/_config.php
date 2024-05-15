f18b01c964d1c9042383bf0419ee6ab259a14f41
milanegri <milan.egri20@gmail.com>
2024-05-15 18:47:00 +0200

    add config.php sample

---
+++ backend/_config.php
@@ -0,0 +1,8 @@
+<?php
+$host= '[DB_HOST_NAME]';
+$user= '[DB_USERNAME]';
+$password= '[DB_PASSWORD]';
+$database= '[DB_DATABASE]';
+$conn = mysqli_connect($host,$user,$password,$database) or die('connection failed');
+
+?>
